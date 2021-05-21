aacf059ca19d6db6d018da2fba5a67458fb7314c
Eduardo Henrique Cazé de Almeida <66096920+Budcaze@users.noreply.github.com>
2021-05-21 00:26:23 -0300

    commit

--- cities-api/src/main/java/com/github/budcaze/citiesapi/cities/CityResource.java
+++ cities-api/src/main/java/com/github/budcaze/citiesapi/cities/CityResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("cities")
+@RequestMapping("/cidades")
 public class CityResource {
 
     private final CityRepository repository;
